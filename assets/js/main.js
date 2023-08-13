7f1029a2205b9f64072de45c70e035a494da8f51
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-08-13 10:07:00 +0300

    Wrote getJoke function. It successfully console logs all data fetched

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
+function getJoke() {
+  const url = 'https://v2.jokeapi.dev/joke/Any';
 
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response not ok');
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      console.log('Data:', data);
+    })
+    .catch((error) => {
+      console.error('Fetch error', error);
+    });
+}
